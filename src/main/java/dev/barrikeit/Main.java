624722e5ee3a90a5019b7d360d7ab201155170d4
Alberto Barral <abarraltigeras@gmail.com>
2026-06-28 23:14:14 +0200

    updated core project and swapped to slf4j

--- src/main/java/dev/barrikeit/Main.java
+++ src/main/java/dev/barrikeit/Main.java
@@ -1,23 +1,23 @@
 package dev.barrikeit;
 
 import dev.barrikeit.config.ApplicationProperties;
-import dev.barrikeit.security.config.SecurityProperties;
-import lombok.extern.log4j.Log4j2;
+import dev.barrikeit.logger.LoggerConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@Log4j2
+@Slf4j
 @SpringBootApplication
 @EnableConfigurationProperties({
   ApplicationProperties.GenericProperties.class,
   ApplicationProperties.ServerProperties.class,
   ApplicationProperties.MailProperties.class,
-  SecurityProperties.class,
 })
 public class Main extends SpringBootServletInitializer {
   public static void main(String[] args) {
+    LoggerConfig.configure();
     SpringApplication.run(Main.class, args);
   }
 }
